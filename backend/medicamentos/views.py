7787d77fd1e76e957510c8694262116e6f946b71
Camana Nick <camananick@gmail.com>
2026-05-08 22:15:19 -0500

    feat(backend): mejoras en modelos, serializers y views de medicamentos

--- backend/medicamentos/views.py
+++ backend/medicamentos/views.py
@@ -12,21 +12,25 @@ class LaboratorioViewSet(viewsets.ModelViewSet):
     queryset = Laboratorio.objects.all()
     serializer_class = LaboratorioSerializer
     permission_classes = [IsAlmacenero]
+    pagination_class = None
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
     permission_classes = [IsAlmacenero]
+    pagination_class = None
 
 class PresentacionViewSet(viewsets.ModelViewSet):
     queryset = Presentacion.objects.all()
     serializer_class = PresentacionSerializer
     permission_classes = [IsAlmacenero]
+    pagination_class = None
 
 class UnidadViewSet(viewsets.ModelViewSet):
     queryset = Unidad.objects.all()
     serializer_class = UnidadSerializer
     permission_classes = [IsAlmacenero]
+    pagination_class = None
 
 class StockMedicamentoViewSet(viewsets.ModelViewSet):
     queryset = StockMedicamento.objects.all()
@@ -41,8 +45,9 @@ class MedicamentoViewSet(viewsets.ModelViewSet):
     search_fields = ['nombre']
     
     def create(self, request, *args, **kwargs):
-        cantidad_inicial = request.data.pop('cantidad_inicial', 0)
-        serializer = self.get_serializer(data=request.data)
+        data = request.data.copy()
+        cantidad_inicial = data.pop('cantidad_inicial', 0)
+        serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         
@@ -54,3 +59,21 @@ class MedicamentoViewSet(viewsets.ModelViewSet):
         
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        data = request.data.copy()
+        stock_value = data.pop('stock', None)
+        
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if stock_value is not None:
+            StockMedicamento.objects.update_or_create(
+                id_medicamento=instance,
+                defaults={'cantidad': int(stock_value)}
+            )
+
+        return Response(serializer.data)
